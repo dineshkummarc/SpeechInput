35d3c8cc4e199caaa7a1991cdcf34e0be40356e1
Polikis <contacto.mail99@gmail.com>
2012-02-15 03:25:25 +0100

    Added basic SpeechAPI to scpeech.js

---
+++ speech.js
@@ -0,0 +1,19 @@
+var speech = {
+	init:function(){
+		var input = document.querySelectorAll('input.speech');
+		for (var i = 0; i < input.length; i++) {
+			if("webkitSpeech" in input[i]){
+				input[i].webkitSpeech = "true";
+				speech.event(input[i]);
+			}
+		}
+	},
+	event:function(target){
+		target.onwebkitspeechchange = function(){
+			target.dataset.speech = target.value;
+			target.value = "";
+			target.blur();
+			alert(target.dataset.speech);
+		}	
+	}
+};
