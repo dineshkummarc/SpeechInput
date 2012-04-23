0b5ddcbb82e037a7a5f14b63474f944afbfeb123
Polikis <contacto.mail99@gmail.com>
2012-04-23 22:53:48 +0200

    Full API Update
    Version 2.0. New Features Include:
    - Object oriented approach, cleaner code
    - Easier to use implementation, very little code written by developer
    - the API in now fully functional, with a callback the user can define
    - Basic Error Handler with costume error callback available
    - New Helper Functions, most notoriously Youtube implementation
    - Other services coming soon: WolframAlpha, Vimeo, Flicker…

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-speech.init();
+var API = new Speech({
+	target:"input[type='speech']",
+	callback:function(input){
+		console.log(input);
+		API.youtube({
+			query:input,
+			max:10,
+			callback:function(data){
+				console.log(data);
+			}
+		});
+	},
+	error:function(error){
+		alert("Error:" + error + "!!");
+	}
+});
