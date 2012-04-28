cc7f586dd467df0653fd4f4d2552fee1036bcb4a
Polikis <contacto.mail99@gmail.com>
2012-04-28 23:11:32 +0200

    Updated the Error Handler and started working on Facebook Implementation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var API = new Speech({
 	target:"input[type='speech']",
 	callback:function(input){
 		console.log(input);
-		API.youtube({
+		API.facebook({
 			query:input,
 			max:10,
 			callback:function(data){
